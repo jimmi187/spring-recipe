f395777ff45298c014de364991942409320eab07
jimmi187 <project.jimi@proton.me>
2023-01-28 20:22:39 +0100

    uni directional relationship  fetch is gonna fetch it every time

--- src/main/java/com/jimi/recipe/domain/Ingredient.java
+++ src/main/java/com/jimi/recipe/domain/Ingredient.java
@@ -12,9 +12,49 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-
     @ManyToOne         // no cascade we don't wanna delete recipe if we delete ingredient
     private Recipe recipe;
 
+    @OneToOne(fetch = FetchType.EAGER)  // uni directional relationship  fetch is gonna fetch it every time
+    private UnitOfMesure uom;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public UnitOfMesure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMesure uom) {
+        this.uom = uom;
+    }
 }
