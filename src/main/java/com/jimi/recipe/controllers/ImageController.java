92dea339b769c3363e214275fd1c423bc23bd6a4
jimmi187 <project.jimi@proton.me>
2023-02-05 02:17:05 +0100

    display image from db

--- src/main/java/com/jimi/recipe/controllers/ImageController.java
+++ src/main/java/com/jimi/recipe/controllers/ImageController.java
@@ -1,8 +1,12 @@
 package com.jimi.recipe.controllers;
 
+import com.jimi.recipe.commands.RecipeCommand;
 import com.jimi.recipe.services.ImageService;
 import com.jimi.recipe.services.RecipeService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Slf4j
 @Controller
@@ -32,7 +40,25 @@ public class ImageController {
     public String handleImagePost(@PathVariable String id,
                                   @RequestParam("imagefile")MultipartFile file){
         imageService.saveImageFile(Long.valueOf(id),file);
-        return "redirect:/recipe/"+id+"show";
+        return "redirect:/recipe/"+id+"/show";
     }
 
+    @GetMapping("recipe/{id}/recipeimage")
+    public void renderImageFromDB(@PathVariable String id,
+                                  HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+
+        if (recipeCommand.getImage() != null) {
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+            int i = 0;
+
+            for (Byte wrappedByte : recipeCommand.getImage()){
+                byteArray[i++] = wrappedByte; //auto unboxing
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is, response.getOutputStream());
+        }
+    }
 }
